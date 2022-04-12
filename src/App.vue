6ace4fecfe86e4fa98523589bbb10af24c1978cc
DmitriyBazurin <bazurin.dmitriy@mail.ru>
2022-04-12 11:34:49 +0300

    create posts

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,106 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="app">
+        <form @submit.prevent>
+            <h4>Создание поста</h4>
+            <input 
+                v-bind:value="title" 
+                @input="title = $event.target.value"
+                class="input" 
+                type="text" 
+                placeholder="Название"
+            >
+            <input 
+                v-bind:value="body" 
+                @input="body = $event.target.value"
+                class="input" 
+                type="text" 
+                placeholder="Описание"
+            >
+            <button 
+                class="btn"
+                @click="createPost"
+            >Создать пост
+            </button>
+        </form>
+        <div class="post" v-for="post in posts">
+            <div><strong>Название</strong>{{post.title}}</div>
+            <div><strong>Описание</strong>{{post.body}}</div>
+        </div>
+    </div>
+    
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+    data() {
+        return {
+            posts: [
+                {id: 1, title: 'Javascript', body: 'Описание поста'},
+                {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+                {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
+                {id: 4, title: 'Javascript 4', body: 'Описание поста 4'},
+            ],
+            title: '',
+            body: ''
+        }
+
+    },
+    methods: {
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost);
+            this.title = '';
+            this.body = '';
+        }
+        
+        
+
+    }
 }
 
-nav {
-  padding: 30px;
+</script>
+
+<style >
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+form {
+    display: flex;
+    flex-direction: column;
 }
+
+.input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+
+.btn {
+    align-self: flex-end;
+    margin-top: 15px;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
+
+.app {
+    padding: 20px;
+}
+
+
 </style>
