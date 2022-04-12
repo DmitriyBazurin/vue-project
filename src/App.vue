075084643c55122e9eb7b52b43c9716470c079d0
DmitriyBazurin <bazurin.dmitriy@mail.ru>
2022-04-12 13:32:26 +0300

    components
    props posts and create componetrs

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,24 @@
 <template>
     <div class="app">
-        <form @submit.prevent>
-            <h4>Создание поста</h4>
-            <input 
-                v-bind:value="title" 
-                @input="title = $event.target.value"
-                class="input" 
-                type="text" 
-                placeholder="Название"
-            >
-            <input 
-                v-bind:value="body" 
-                @input="body = $event.target.value"
-                class="input" 
-                type="text" 
-                placeholder="Описание"
-            >
-            <button 
-                class="btn"
-                @click="createPost"
-            >Создать пост
-            </button>
-        </form>
-        <div class="post" v-for="post in posts">
-            <div><strong>Название</strong>{{post.title}}</div>
-            <div><strong>Описание</strong>{{post.body}}</div>
-        </div>
+        <post-form
+            @create="createPost"
+        />
+        <post-list 
+            :posts="posts"
+            
+        />
     </div>
-    
 </template>
 
 <script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+
 export default {
+    components: {
+        PostList, PostForm
+    },
+
     data() {
         return {
             posts: [
@@ -40,25 +27,13 @@ export default {
                 {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
                 {id: 4, title: 'Javascript 4', body: 'Описание поста 4'},
             ],
-            title: '',
-            body: ''
         }
 
     },
     methods: {
-        createPost() {
-            const newPost = {
-                id: Date.now(),
-                title: this.title,
-                body: this.body,
-            }
-            this.posts.push(newPost);
-            this.title = '';
-            this.body = '';
-        }
-        
-        
-
+        createPost(post) {
+            this.posts.push(post);
+        },
     }
 }
 
@@ -71,32 +46,8 @@ export default {
     box-sizing: border-box;
 }
 
-.post {
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-}
 
-form {
-    display: flex;
-    flex-direction: column;
-}
 
-.input {
-    width: 100%;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    margin-top: 15px;
-}
-
-.btn {
-    align-self: flex-end;
-    margin-top: 15px;
-    padding: 10px 15px;
-    background: none;
-    color: teal;
-    border: 1px solid teal;
-}
 
 .app {
     padding: 20px;
