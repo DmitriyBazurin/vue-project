c6c53b20de4bc50eb8001068d04af20a35cc709e
DmitriyBazurin <bazurin.dmitriy@mail.ru>
2022-04-13 11:09:18 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,27 @@
 <template>
-    <div class="app">
-        <post-form
-            @create="createPost"
-        />
-        <post-list 
-            :posts="posts"
-            
-        />
-    </div>
+  <navbar></navbar>
+  <div class="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import PostForm from "@/components/PostForm.vue";
-import PostList from "@/components/PostList.vue";
+import Navbar from "@/components/Navbar";
 
 export default {
-    components: {
-        PostList, PostForm
-    },
-
-    data() {
-        return {
-            posts: [
-                {id: 1, title: 'Javascript', body: 'Описание поста'},
-                {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
-                {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
-                {id: 4, title: 'Javascript 4', body: 'Описание поста 4'},
-            ],
-        }
-
-    },
-    methods: {
-        createPost(post) {
-            this.posts.push(post);
-        },
-    }
+  components: {
+    Navbar
+  }
 }
-
 </script>
 
-<style >
+<style>
 * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-
-
-
-
 .app {
-    padding: 20px;
+  padding: 20px;
 }
-
-
 </style>
